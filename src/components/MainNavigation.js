8a16deee4e944a789ace18c6daa084494cddfd79
hoanglamvu04 <hoanglamvuytb@gmail.com>
2025-04-09 09:24:49 +0700

    ok

--- src/components/MainNavigation.js
+++ src/components/MainNavigation.js
@@ -4,21 +4,22 @@ const Menu = () => {
   return (
     <div className="nav-scroll-wrapper">
   <nav className="main-nav">
-    <a href="#">Trang Chủ</a>
-    <a href="/category/">Sản Phẩm Nổi Bật</a>
-    <a href="#">Hàng Mới Về</a>
-    <a href="#">Áo Nam</a>
-    <a href="#">Áo Nữ</a>
-    <a href="#">Quần Nam</a>
-    <a href="#">Quần Nữ</a>
-    <a href="#">Váy</a>
-    <a href="#">Giày Dép</a>
-    <a href="#">Túi Xách</a>
-    <a href="#">Mũ & Nón</a>
-    <a href="#">Phụ Kiện</a>
-    <a href="#">Đồ Bộ</a>
-    <a href="#">Áo Len</a>
-    <a href="#">Giảm Giá</a>
+  <a href="/" className="menu-item">Trang Chủ</a>
+      <a href="/category/" className="menu-item">Sản Phẩm Nổi Bật</a>
+      <a href="/category/hang-moi-ve" className="menu-item">Hàng Mới Về</a>
+      <a href="/category/ao-nam" className="menu-item">Áo Nam</a>
+      <a href="/category/ao-nu" className="menu-item">Áo Nữ</a>
+      <a href="/category/quan-nam" className="menu-item">Quần Nam</a>
+      <a href="/category/quan-nu" className="menu-item">Quần Nữ</a>
+      <a href="/category/vay" className="menu-item">Váy</a>
+      <a href="/category/giay-dep" className="menu-item">Giày Dép</a>
+      <a href="/category/tui-xach" className="menu-item">Túi Xách</a>
+      <a href="/category/mu-non" className="menu-item">Mũ & Nón</a>
+      <a href="/category/phu-kien" className="menu-item">Phụ Kiện</a>
+      <a href="/category/do-bo" className="menu-item">Đồ Bộ</a>
+      <a href="/category/ao-len" className="menu-item">Áo Len</a>
+      <a href="/category/giam-gia" className="menu-item">Giảm Giá</a>
+      <a href="#" className="menu-item">...</a>
   </nav>
 </div>
 
