9b9ad7e670b5f2fabf1250c65bd0f19bcae1cd63
hoanglamvu04 <hoanglamvuytb@gmail.com>
2025-04-09 09:54:17 +0700

    ioo

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,19 +1,56 @@
-import React from "react";
-import "../styles/Banner.css"; 
+import React, { useState } from "react";
+import "../styles/Banner.css";
 
 const Banner = () => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const largeImages = [
+    "https://cf.shopee.vn/file/vn-11134258-7ras8-m5184szf0klz56_xxhdpi",
+    "https://cf.shopee.vn/file/vn-11134258-7ra0g-m8bm8vahff5z8c_xxhdpi",
+    "https://cf.shopee.vn/file/vn-11134258-7ra0g-m85x5kqjfbla5e_xxhdpi"
+  ];
+
+  const smallImages = [
+ "https://cf.shopee.vn/file/vn-11134258-7ras8-m5184szf0klz56_xxhdpi",
+ "https://cf.shopee.vn/file/vn-11134258-7ras8-m5184szf0klz56_xxhdpi"
+  ];
+
+  const handlePrev = () => {
+    setCurrentIndex((prev) => (prev === 0 ? largeImages.length - 1 : prev - 1));
+  };
+
+  const handleNext = () => {
+    setCurrentIndex((prev) => (prev === largeImages.length - 1 ? 0 : prev + 1));
+  };
+
   return (
     <div className="banner-container">
       <div className="banner-grid">
-        {/* Ảnh lớn bên trái */}
+        {/* Ảnh lớn bên trái có điều hướng */}
         <div className="banner-large">
-        <img src="/assets/images/banner1.1.jpg" alt="Banner 1" className="banner-item" />
+          <img
+            src={largeImages[currentIndex]}
+            alt={`Banner ${currentIndex + 1}`}
+            className="banner-item"
+          />
+          <button className="banner-arrow left" onClick={handlePrev}>
+            ❮
+          </button>
+          <button className="banner-arrow right" onClick={handleNext}>
+            ❯
+          </button>
         </div>
 
-        {/* Cột chứa 2 ảnh nhỏ */}
+        {/* Cột chứa 2 ảnh nhỏ bên phải */}
         <div className="banner-small">
-        <img src="/assets/images/banner1.jpg" alt="Banner 1" className="banner-item" />
-        <img src="/assets/images/banner1.jpg" alt="Banner 1" className="banner-item" />
+          {smallImages.map((img, index) => (
+            <img
+              key={index}
+              src={img}
+              alt={`Small Banner ${index + 1}`}
+              className="banner-item"
+            />
+          ))}
         </div>
       </div>
     </div>
